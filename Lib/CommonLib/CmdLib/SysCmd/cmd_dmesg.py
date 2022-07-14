9817b2a8c4cc308cf445048eb8132d6af2bc687f
wangkai <17875822605@163.com>
2022-07-14 23:59:57 +0800

    update

--- Lib/CommonLib/CmdLib/SysCmd/cmd_dmesg.py
+++ Lib/CommonLib/CmdLib/SysCmd/cmd_dmesg.py
@@ -4,3 +4,10 @@
 # @Author  : v_bkaiwang
 # @File    : cmd_dmesg.py
 # @Software: win10 Tensorflow1.13.1 python3.6.3
+from Lib.CommonLib.BaseLib.log_message import LogMessage, LOG_ERROR
+from Lib.CommonLib.BaseLib.terminal import Terminal
+
+
+class CmdDmesg:
+    def __init__(self, terminal=None):
+        pass
