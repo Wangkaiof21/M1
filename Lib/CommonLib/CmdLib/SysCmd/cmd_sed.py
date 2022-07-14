2b2ce7db020dd49504e45dbf67b5575d9801baae
wangkai <17875822605@163.com>
2022-07-14 23:57:21 +0800

    update

--- Lib/CommonLib/CmdLib/SysCmd/cmd_sed.py
+++ Lib/CommonLib/CmdLib/SysCmd/cmd_sed.py
@@ -41,3 +41,42 @@ def cmd_add(index, content, file):
     """
     return r'sed {}a {} {} -i'.format(index, content, file)
 
+
+@safe_mode
+def cmd_delete(index, file):
+    """
+    删除指定位置
+    :param index:指定行号:为'$'时，删除最后一行.为'n,m'时，表示删除n-m行
+    :param file:操作文件
+    :return:
+    """
+    return r'sed "{}d #" {} -i'.format(index, file)
+
+
+@safe_mode
+def cmd_change(index, content, file):
+    """
+    将指定位置的值修改成content
+    :param index: 指定行号:为'$'时，修改最后一行.为'n,m'时，表示将n-m行的内容用content替换
+    :param content: 替换内容
+    :param file: 文件
+    :return:
+    """
+    return r"sed '{}c|{}' {} -i".format(index, content, file)
+
+
+@safe_mode
+def cmd_supersede(_old, _new, file, greed_mode=True, line_range=''):
+    """
+    将旧内容替换成新内容
+    :param _old: 旧内容
+    :param _new: 新内容
+    :param file: 操作文件
+    :param greed_mode: 贪婪模式
+    :param line_range: 修改的行范围
+    :return:
+    """
+    return r"sed -r '{}s|{}|{}|g' {} -i".format(line_range, _old, _new,
+                                                file) if greed_mode else r"sed -r '{}s|{}|{}|' {} -i".format(line_range,
+                                                                                                             _old, _new,
+                                                                                                             file)
