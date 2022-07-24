8c89643d1f648c53a45eeba577f6f2c0f91d35fb
wangkai <17875822605@163.com>
2022-07-24 22:46:44 +0800

    dmesg update

--- Lib/CommonLib/CmdLib/SysCmd/cmd_dmesg.py
+++ Lib/CommonLib/CmdLib/SysCmd/cmd_dmesg.py
@@ -131,3 +131,83 @@ class CmdDmesg:
     def read_clean(self):
         option = "-c"
         return self.grep_get(option)
+
+    # 清除内核缓冲区
+    def clear(self):
+        option = "-C"
+        return self.grep_get(option)
+
+    # 显示帮助信息
+    def help(self):
+        option = "-h"
+        return self.grep_get(option)
+
+    # 显示dmesg日志后n行
+    def tail(self, lines_number=20):
+        option = "| tail -" + str(lines_number)
+        return self.grep_get(option)
+
+    # 列出加载到内核的所有驱动
+    def more(self):
+        option = "| more"
+        return self.grep_get(option)
+
+    def less(self):
+        option = "| less"
+        return self.grep_get(option)
+
+    # 显示打印信息之间的时间差
+    def show_time_delta(self):
+        option = "-d"
+        return self.grep_get(option)
+
+    # 显示本地时间和时间增量
+    def reltime(self):
+        option = "-e"
+        return self.grep_get(option)
+
+    # 显示内核消息
+    def kernel_msg_display(self):
+        option = "-k"
+        return self.grep_get(option)
+
+    # 打印原始信息缓冲
+    def raw_message_buffer(self):
+        option = "-r"
+        return self.grep_get(option)
+
+    # 强制使用syslog(2)而不是/dev/kmsg
+    def force_use_syslog2(self):
+        option = "-S"
+        return self.grep_get(option)
+
+    # 显示人类可读的时间戳
+    def timestamp_show(self):
+        option = "-T"
+        return self.grep_get(option)
+
+    # 显示用户信息
+    def user_info_display(self):
+        option = "-u"
+        return self.grep_get(option)
+
+    # 输出版本信息并退出
+    def version_info_display(self):
+        option = "-V"
+        return self.grep_get(option)
+
+    # 打印输出最近一次信息
+    def last_msg_output(self):
+        option = "| tail -f"
+        return self.grep_get(option)
+
+    # 将系统启动信息保存在文件中
+    def file_saved(self, file="boot.messages"):
+        option = "> {}".format(file)
+        return self.grep_get(option)
+
+
+if __name__ == '__main__':
+    term = Terminal(host_type="ssh", host_ip='192.168.11.141', port=22, username='root', password='123123', proxy=True)
+    term.connect()
+    dmesg = CmdDmesg(term)
