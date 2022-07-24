ba46e187ea24e17defb7f538336c3b8fa438ca46
wangkai <17875822605@163.com>
2022-07-24 23:00:11 +0800

    dmesg update

--- Lib/CommonLib/CmdLib/SysCmd/cmd_numactl_taskset.py
+++ Lib/CommonLib/CmdLib/SysCmd/cmd_numactl_taskset.py
@@ -4,3 +4,14 @@
 # @Author  : v_bkaiwang
 # @File    : cmd_numactl_taskset.py
 # @Software: win10 Tensorflow1.13.1 python3.6.3
+
+class CmdNumactl:
+    def numactl_bindcore(self, phy_cpu_bind=None, mem_bind=None):
+        """
+        进行core跨Nodes测试
+        :param phy_cpu_bind: 0~127
+        :param mem_bind: 0~3
+        :return:
+        举例 phy_cpu_bind=0,mem_bind=0 表示core 0 访问node0 的内存
+        """
+        return '' if not phy_cpu_bind else self.__numactl(phy_cpu_bind=phy_cpu_bind, mem_bind=mem_bind)
